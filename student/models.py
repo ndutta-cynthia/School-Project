3280cf30c9532295a3eb84b361429b5de7fb18a5
ndutta-cynthia <nduttamaina@gmail.com>
2024-06-14 19:45:21 +0300

    created files

---
+++ student/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length= 20)
+    last_name = models.CharField(max_length= 20)
+    email= models.EmailField()
+    code = models.PositiveSmallIntegerField()
+    date_of_birth= models.DateField()
+    country= models.CharField(max_length= 20)
+    bio= models.TextField()
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
